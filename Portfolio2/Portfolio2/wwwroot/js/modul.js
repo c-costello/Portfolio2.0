a555a36ef3d9091ac3647acd8889a6514b140bd4
Clarice Costello <claricostello@outlook.com>
2019-05-30 13:17:11 -0700

    added modals to all seven image tiles

--- Portfolio2/Portfolio2/wwwroot/js/modul.js
+++ Portfolio2/Portfolio2/wwwroot/js/modul.js
@@ -3,6 +3,32 @@ $(".img-one").on("click", () => {
     $(".modal-one").addClass("is-active");
 })
 
+$(".img-two").on("click", () => {
+    $(".modal-two").addClass("is-active");
+})
+
+$(".img-three").on("click", () => {
+    $(".modal-three").addClass("is-active");
+})
+
+$(".img-four").on("click", () => {
+    $(".modal-four").addClass("is-active");
+})
+
+$(".img-five").on("click", () => {
+    $(".modal-five").addClass("is-active");
+})
+
+$(".img-six").on("click", () => {
+    $(".modal-six").addClass("is-active");
+})
+
+$(".img-seven").on("click", () => {
+    $(".modal-seven").addClass("is-active");
+})
+
+
+
 $(".modal-close").on("click", () => {
-    $(".modal-one").removeClass("is-active");
+    $(".modal").removeClass("is-active");
 })
