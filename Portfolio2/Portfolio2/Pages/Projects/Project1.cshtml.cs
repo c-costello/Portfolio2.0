6f903c0087be76ea8c0d498849ae2d024759784b
Clarice Costello <claricostello@outlook.com>
2019-05-30 09:27:11 -0700

    Scaffolded file structure

---
+++ Portfolio2/Portfolio2/Pages/Projects/Project1.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio2.Pages.Projects
+{
+    public class Project1Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Portfolio2/Portfolio2/Pages/Projects/Project4.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio2.Pages.Projects
+{
+    public class Project4Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
